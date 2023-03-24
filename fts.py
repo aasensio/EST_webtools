d55ec2ee815622999723d420cd8a9c93ac692a50
cruiz <claudia.ruizdegalarretafanjul@ias.u-psud.fr>
2023-03-24 11:08:32 +0000

    Add new atlas and enhancing module

--- fts.py
+++ fts.py
@@ -1,28 +1,54 @@
 import numpy as np
 from scipy.io import readsav
 
-class fts:
+DATAFILES = [
+    './fts_disk_center.idlsave',
+'./ftscnt_full.sav']
 
-    ll = None
-    ii = None
-    cc = None
-    nu = None
+CLIGHT = 2.99792458e8  # speed of light [m/s]
+AA_TO_M = 1e-10
+CM_TO_M = 1e-2
+ERG_TO_WATT = 1e-7
 
-    datafile = './fts_disk_center.idlsave'
+class fts(object):
 
     def __init__(self):
-        
-        # watt / (cm2 ster AA)  as emitted at solar surface
-        t = readsav(self.datafile)
-        
-        # convert to J s-1 m-2 m-1 sr-1
+        self.ll = None
+        self.ii = None
+        self.cc = None
+        self.nu = None
 
-        clight=2.99792458e8         #speed of light [m/s]                                  
-        aa_to_m=1e-10                                                                        
-        cm_to_m=1e-2                       
-        
-        self.ll = t['ftswav']* aa_to_m
-        self.nu = clight / self.ll
-        self.ii = t['ftsint'] * cm_to_m**(-2) * aa_to_m**(-1) # from from W /( cm2 ster AA) to  W / (m2 ster m)  
-        self.cc = t['ftscnt'] * cm_to_m**(-2) * aa_to_m**(-1) 
+        self.datafile = './fts_disk_center.idlsave'
+
+    def get_atlas(self, atlas=0):
+
+        # The atlas parameter defines if the reference atlas is the original from J. Leenarts, expressed in watt/(cm2 ster AA)
+        # as emitted at solar surface
+        # Or the new atlas exported by M. Collados allowing for going beyong in the IR spectrum, and expressed
+        # in erg/s/(cm2sterAA)
+        self.datafile = DATAFILES[atlas]
+        t = readsav(self.datafile)
+
+        if atlas == 0:
+            # convert to J s-1 m-2 m-1 sr-1
+            self.ll = t['ftswav'] * AA_TO_M
+            self.nu = CLIGHT / self.ll
+            self.ii = t['ftsint'] * CM_TO_M ** (-2) * AA_TO_M ** (-1)  # from from W /( cm2 ster AA) to  W / (m2 ster m)
+            self.cc = t['ftscnt'] * CM_TO_M ** (-2) * AA_TO_M ** (-1)
+
+        else:
+            ### for ftscnt_full.sav
+            ## lam: wavelength in A  with 2 mA steps. IR data has been interpolated as the step was 4 mA.
+            ## ftsnorm:spectrum normalized to the continuum
+            ## cont: value of the continuum at all wavelengths.
+            # The spectro in erg/s/cm2/A/sr) is the product of ftsnorm and cont.
+            self.ll = t["lam"] * AA_TO_M
+            cont = t["cont"]
+            ftsnorm = t["ftsnorm"]
+            self.ii = cont * ftsnorm * ERG_TO_WATT * CM_TO_M ** (-2) * AA_TO_M ** (-1)
+
+
+
+
+
        
